0dced28fb7110694064e4b1491d3afbfb59578f5
Yiwen Zhu <yiwzhu@seas.upenn.edu>
2020-10-15 20:34:36 +0100

    Add getter and setter methods for ItemToPurchase

--- cit591_vercion_control_ex922/src/cit591_vercion_control_ex922/ItemToPurchase.java
+++ cit591_vercion_control_ex922/src/cit591_vercion_control_ex922/ItemToPurchase.java
@@ -4,4 +4,28 @@ public class ItemToPurchase {
 	String itemName;
 	int itemPrice;
 	int itemQuantity;
+	
+	String getName() {
+		return itemName;
+	}
+	
+	int getPrice() {
+		return itemPrice;
+	}
+	
+	int getQuantity() {
+		return itemQuantity;
+	}
+	
+	public void setName(String name) {
+		this.itemName = name;
+	}
+	
+	public void setPrice(int price) {
+		this.itemPrice = price;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.itemQuantity = quantity;
+	}
 }
